c460da7665f6f2978cd4b3e2a8425a15b4926de5
reid <reidbras@gmail.com>
2023-07-29 10:48:32 -0500

    Adding new view for new products url and renaming index view to products.

--- products/views.py
+++ products/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse('<h1 style="text-align:center; color: blue">Hello World</h1>')
+def products(request):
+    return HttpResponse('<h1 style="text-align:center; color: blue">Products</h1>')
+
+def new_products(request):
+    return HttpResponse('<h1 style="text-align:center; color: green">New Products</h1>')
